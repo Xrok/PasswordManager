348969ac045350de6ad97ac62f5bfaa4ea6d454d
AlejandroGH97 <AlejandroGH97@users.noreply.github.com>
2021-07-19 19:54:43 -0500

    Added copy button to register alert.

--- lib/widgets/register_alert.dart
+++ lib/widgets/register_alert.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:password_manager/config/theme/colors.dart';
 import 'package:password_manager/constants/app_constants.dart';
 
 class RegisterAlert extends StatelessWidget {
-  const RegisterAlert({Key? key, required this.continueCallback, required this.secret}) : super(key: key);
+  const RegisterAlert(
+      {Key? key, required this.continueCallback, required this.secret})
+      : super(key: key);
 
   final Function continueCallback;
   final String secret;
@@ -15,28 +18,46 @@ class RegisterAlert extends StatelessWidget {
         Text(
           'Llave de encriptación',
           textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.headline3?.copyWith(color: CustomColors.cultured),
+          style: Theme.of(context)
+              .textTheme
+              .headline3
+              ?.copyWith(color: CustomColors.cultured),
         ),
         SizedBox(
           height: AppConstants.sizes[2],
         ),
         Text(
             'Esta llave será solicitada cuando ingreses desde un nuevo dispositivo, puedes escribirla en algún lugar y ademas será guardada en la aplicación mediante un código QR.',
-            style: Theme.of(context).textTheme.bodyText1?.copyWith(color: CustomColors.cultured),
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                ?.copyWith(color: CustomColors.cultured),
             textAlign: TextAlign.justify),
         SizedBox(
           height: AppConstants.sizes[0],
         ),
         Text(secret,
-            style: Theme.of(context).textTheme.bodyText1?.copyWith(color: CustomColors.cultured),
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                ?.copyWith(color: CustomColors.cultured),
             textAlign: TextAlign.justify),
         SizedBox(
           height: AppConstants.sizes[2],
         ),
-        ElevatedButton(
-          child: Text('Continuar'),
-          onPressed: () => {continueCallback(context)},
-        )
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          ElevatedButton(
+            child: Text('Copiar'),
+            onPressed: () => {Clipboard.setData(ClipboardData(text: secret))},
+          ),
+          SizedBox(
+            width: AppConstants.sizes[2],
+          ),
+          ElevatedButton(
+            child: Text('Continuar'),
+            onPressed: () => {continueCallback(context)},
+          )
+        ]),
       ],
     );
   }
